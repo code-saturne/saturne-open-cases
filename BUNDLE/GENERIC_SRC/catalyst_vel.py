669c279bb1bfcd669728a5b0911c18857b913df3
Yvan Fournier <yvan.fournier@noreply.edf.fr>
2025-06-26 00:59:46 +0200

    BUNDLE: add missing files.

---
+++ BUNDLE/GENERIC_SRC/catalyst_vel.py
@@ -0,0 +1,170 @@
+# script-version: 2.0
+# Catalyst state generated using paraview version 5.11.0
+
+#### import the simple module from the paraview
+from paraview.simple import *
+#### disable automatic camera reset on 'Show'
+paraview.simple._DisableFirstRenderCameraReset()
+
+# ----------------------------------------------------------------
+# setup views used in the visualization
+# ----------------------------------------------------------------
+
+# get the material library
+materialLibrary1 = GetMaterialLibrary()
+
+# Create a new 'Render View'
+renderView1 = CreateView('RenderView')
+renderView1.ViewSize = [880, 657]
+renderView1.AxesGrid = 'GridAxes3DActor'
+renderView1.CenterOfRotation = [0.0, 0.0, 0.012749999761581421]
+renderView1.StereoType = 'Crystal Eyes'
+renderView1.CameraPosition = [0.0, 0.0, 0.41917079720253014]
+renderView1.CameraFocalPoint = [0.0, 0.0, 0.012749999761581421]
+renderView1.CameraFocalDisk = 1.0
+renderView1.CameraParallelScale = 0.12727922567120037
+renderView1.BackEnd = 'OSPRay raycaster'
+renderView1.OSPRayMaterialLibrary = materialLibrary1
+
+SetActiveView(None)
+
+# ----------------------------------------------------------------
+# setup view layouts
+# ----------------------------------------------------------------
+
+# create new layout object 'Layout #1'
+layout1 = CreateLayout(name='Layout #1')
+layout1.AssignView(0, renderView1)
+layout1.SetSize(880, 657)
+
+# ----------------------------------------------------------------
+# restore active view
+SetActiveView(renderView1)
+# ----------------------------------------------------------------
+
+# ----------------------------------------------------------------
+# setup the data processing pipelines
+# ----------------------------------------------------------------
+
+# create a new 'EnSight Reader'
+rESULTS_FLUID_DOMAINcase = EnSightReader(registrationName='RESULTS_FLUID_DOMAIN.case', CaseFileName='/home/D43345/Code_Saturne/Validation/saturne-open-cases/BUNDLE/BENCH_C016_04/RESU/gpu/postprocessing/RESULTS_FLUID_DOMAIN.case')
+rESULTS_FLUID_DOMAINcase.CellArrays = ['Velocity', 'Pressure', 'k', 'epsilon', 'TurbVisc', 'CourantNb', 'FourierNb', 'total_pressure', 'Local_Time_Step']
+
+# create a new 'Slice'
+slice1 = Slice(registrationName='Slice1', Input=rESULTS_FLUID_DOMAINcase)
+slice1.SliceType = 'Plane'
+slice1.HyperTreeGridSlicer = 'Plane'
+slice1.SliceOffsetValues = [0.0]
+
+# init the 'Plane' selected for 'SliceType'
+slice1.SliceType.Origin = [0.0, 0.0, 0.012749999761581421]
+slice1.SliceType.Normal = [0.0, 0.0, 1.0]
+
+# init the 'Plane' selected for 'HyperTreeGridSlicer'
+slice1.HyperTreeGridSlicer.Origin = [0.0, 0.0, 0.012749999761581421]
+
+# ----------------------------------------------------------------
+# setup the visualization in view 'renderView1'
+# ----------------------------------------------------------------
+
+# show data from slice1
+slice1Display = Show(slice1, renderView1, 'GeometryRepresentation')
+
+# get 2D transfer function for 'Velocity'
+velocityTF2D = GetTransferFunction2D('Velocity')
+
+# get color transfer function/color map for 'Velocity'
+velocityLUT = GetColorTransferFunction('Velocity')
+velocityLUT.TransferFunction2D = velocityTF2D
+velocityLUT.RGBPoints = [0.12359778371932974, 0.231373, 0.298039, 0.752941, 0.7705697354970203, 0.865003, 0.865003, 0.865003, 1.417541687274711, 0.705882, 0.0156863, 0.14902]
+velocityLUT.ScalarRangeInitialized = 1.0
+
+# trace defaults for the display properties.
+slice1Display.Representation = 'Surface'
+slice1Display.ColorArrayName = ['CELLS', 'Velocity']
+slice1Display.LookupTable = velocityLUT
+slice1Display.SelectTCoordArray = 'None'
+slice1Display.SelectNormalArray = 'None'
+slice1Display.SelectTangentArray = 'None'
+slice1Display.OSPRayScaleArray = 'Pressure'
+slice1Display.OSPRayScaleFunction = 'PiecewiseFunction'
+slice1Display.SelectOrientationVectors = 'Velocity'
+slice1Display.ScaleFactor = 0.01800000071525574
+slice1Display.SelectScaleArray = 'Pressure'
+slice1Display.GlyphType = 'Arrow'
+slice1Display.GlyphTableIndexArray = 'Pressure'
+slice1Display.GaussianRadius = 0.0009000000357627869
+slice1Display.SetScaleArray = ['POINTS', 'Pressure']
+slice1Display.ScaleTransferFunction = 'PiecewiseFunction'
+slice1Display.OpacityArray = ['POINTS', 'Pressure']
+slice1Display.OpacityTransferFunction = 'PiecewiseFunction'
+slice1Display.DataAxesGrid = 'GridAxesRepresentation'
+slice1Display.PolarAxes = 'PolarAxesRepresentation'
+slice1Display.SelectInputVectors = ['POINTS', 'Velocity']
+slice1Display.WriteLog = ''
+slice1Display.NoiseArray = ['POINTS', 'Pressure']
+slice1Display.BumpMapInputDataArray = ['POINTS', 'Pressure']
+slice1Display.ExtrusionInputDataArray = ['CELLS', 'Pressure']
+slice1Display.InputVectors = ['CELLS', 'Velocity']
+
+# init the 'PiecewiseFunction' selected for 'ScaleTransferFunction'
+slice1Display.ScaleTransferFunction.Points = [-288.5645751953125, 0.0, 0.5, 0.0, 1399.6429443359375, 1.0, 0.5, 0.0]
+
+# init the 'PiecewiseFunction' selected for 'OpacityTransferFunction'
+slice1Display.OpacityTransferFunction.Points = [-288.5645751953125, 0.0, 0.5, 0.0, 1399.6429443359375, 1.0, 0.5, 0.0]
+
+# setup the color legend parameters for each legend in this view
+
+# get color legend/bar for velocityLUT in view renderView1
+velocityLUTColorBar = GetScalarBar(velocityLUT, renderView1)
+velocityLUTColorBar.Title = 'Velocity'
+velocityLUTColorBar.ComponentTitle = 'Magnitude'
+
+# set color bar visibility
+velocityLUTColorBar.Visibility = 1
+
+# show color legend
+slice1Display.SetScalarBarVisibility(renderView1, True)
+
+# ----------------------------------------------------------------
+# setup color maps and opacity mapes used in the visualization
+# note: the Get..() functions create a new object, if needed
+# ----------------------------------------------------------------
+
+# get opacity transfer function/opacity map for 'Velocity'
+velocityPWF = GetOpacityTransferFunction('Velocity')
+velocityPWF.Points = [0.12359778371932974, 0.0, 0.5, 0.0, 1.417541687274711, 1.0, 0.5, 0.0]
+velocityPWF.ScalarRangeInitialized = 1
+
+# ----------------------------------------------------------------
+# setup extractors
+# ----------------------------------------------------------------
+
+# create extractor
+pNG1 = CreateExtractor('PNG', renderView1, registrationName='PNG1')
+# trace defaults for the extractor.
+pNG1.Trigger = 'TimeStep'
+
+# init the 'PNG' selected for 'Writer'
+pNG1.Writer.FileName = 'velocity_{timestep:06d}.png'
+pNG1.Writer.ImageResolution = [880, 657]
+pNG1.Writer.Format = 'PNG'
+
+# ----------------------------------------------------------------
+# restore active source
+SetActiveSource(pNG1)
+# ----------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+# Catalyst options
+from paraview import catalyst
+options = catalyst.Options()
+options.GlobalTrigger = 'TimeStep'
+options.CatalystLiveTrigger = 'TimeStep'
+
+# ------------------------------------------------------------------------------
+if __name__ == '__main__':
+    from paraview.simple import SaveExtractsUsingCatalystOptions
+    # Code for non in-situ environments; if executing in post-processing
+    # i.e. non-Catalyst mode, let's generate extracts using Catalyst options
+    SaveExtractsUsingCatalystOptions(options)
---
+++ BUNDLE/GENERIC_SRC/cs_user_postprocess.cpp
@@ -0,0 +1,112 @@
+/*============================================================================
+ * Define postprocessing output.
+ *============================================================================*/
+
+/* VERS */
+
+/*
+  This file is part of code_saturne, a general-purpose CFD tool.
+
+  Copyright (C) 1998-2025 EDF S.A.
+
+  This program is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation; either version 2 of the License, or (at your option) any later
+  version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+  Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+/*----------------------------------------------------------------------------*/
+
+#include "base/cs_defs.h"
+
+/*----------------------------------------------------------------------------
+ * Standard C library headers
+ *----------------------------------------------------------------------------*/
+
+#include "stdlib.h"
+#include "string.h"
+
+/*----------------------------------------------------------------------------
+ *  Local headers
+ *----------------------------------------------------------------------------*/
+
+#include "cs_headers.h"
+
+/*----------------------------------------------------------------------------*/
+
+BEGIN_C_DECLS
+
+/*============================================================================
+ * Local (user defined) function definitions
+ *============================================================================*/
+
+static int  snap_id = 0.;
+static const int  n_snaps = 1;
+static const double  t_snap[] = {750.0};
+
+/*============================================================================
+ * User function definitions
+ *============================================================================*/
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Define post-processing writers.
+ *
+ * The default output format and frequency may be configured, and additional
+ * post-processing writers allowing outputs in different formats or with
+ * different format options and output frequency than the main writer may
+ * be defined.
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_user_postprocess_writers(void)
+{
+  /* Use Catalyst writer when available */
+#if defined (HAVE_CATALYST)
+  if (fvm_writer_format_available(fvm_writer_get_format_id("catalyst"))) {
+    cs_post_define_writer(1,                            /* writer_id */
+                          "catalyst",                   /* writer name */
+                          "postprocessing",             /* directory name */
+                          "catalyst",                   /* format_name */
+                          "",                           /* format_options */
+                          FVM_WRITER_TRANSIENT_CONNECT,
+                          false,                        /* output_at_start */
+                          true,                         /* output_at_end */
+                          -1,                           /* frequency_n */
+                          -1.0);                        /* frequency_t */
+  }
+#endif
+}
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \brief Define post-processing meshes.
+ *
+ * The main post-processing meshes may be configured, and additional
+ * post-processing meshes may be defined as a subset of the main mesh's
+ * cells or faces (both interior and boundary).
+ */
+/*----------------------------------------------------------------------------*/
+
+void
+cs_user_postprocess_meshes(void)
+{
+#if defined (HAVE_CATALYST)
+  /* Add Catalyst output to volume mesh when available */
+  cs_post_mesh_attach_writer(CS_POST_MESH_VOLUME, 1);
+#endif
+}
+
+/*----------------------------------------------------------------------------*/
+
+END_C_DECLS
