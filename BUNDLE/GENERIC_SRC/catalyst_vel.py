363b6f5dd0d941daa8d7a038db78323ff9e64b22
Yvan Fournier <yvan.fournier@noreply.edf.fr>
2025-06-26 01:43:00 +0200

    BUNDLE: addition fixes for Catalyst script.

--- BUNDLE/GENERIC_SRC/catalyst_vel.py
+++ BUNDLE/GENERIC_SRC/catalyst_vel.py
@@ -17,14 +17,11 @@ materialLibrary1 = GetMaterialLibrary()
 renderView1 = CreateView('RenderView')
 renderView1.ViewSize = [880, 657]
 renderView1.AxesGrid = 'GridAxes3DActor'
-renderView1.CenterOfRotation = [0.0, 0.0, 0.012749999761581421]
+renderView1.CenterOfRotation = [0.0, 0.0, 0.0]
 renderView1.StereoType = 'Crystal Eyes'
-renderView1.CameraPosition = [0.0, 0.0, 0.41917079720253014]
-renderView1.CameraFocalPoint = [0.0, 0.0, 0.012749999761581421]
+renderView1.CameraPosition = [0.0, 0.0, 0.42]
+renderView1.CameraFocalPoint = [0.0, 0.0, 0.0]
 renderView1.CameraFocalDisk = 1.0
-renderView1.CameraParallelScale = 0.12727922567120037
-renderView1.BackEnd = 'OSPRay raycaster'
-renderView1.OSPRayMaterialLibrary = materialLibrary1
 
 SetActiveView(None)
 
@@ -48,7 +45,7 @@ SetActiveView(renderView1)
 
 # create a new 'EnSight Reader'
 rESULTS_FLUID_DOMAINcase = EnSightReader(registrationName='catalyst', CaseFileName='./postprocessing/RESULTS_FLUID_DOMAIN.case')
-rESULTS_FLUID_DOMAINcase.CellArrays = ['Velocity', 'Pressure', 'k', 'epsilon', 'TurbVisc', 'CourantNb', 'FourierNb', 'total_pressure', 'Local_Time_Step']
+rESULTS_FLUID_DOMAINcase.CellArrays = ['Velocity', 'Pressure', 'k', 'epsilon', 'TurbVisc', 'total_pressure', 'Local_Time_Step']
 
 # create a new 'Slice'
 slice1 = Slice(registrationName='Slice1', Input=rESULTS_FLUID_DOMAINcase)
@@ -78,40 +75,18 @@ velocityLUT = GetColorTransferFunction('Velocity')
 velocityLUT.TransferFunction2D = velocityTF2D
 velocityLUT.RGBPoints = [0.12359778371932974, 0.231373, 0.298039, 0.752941, 0.7705697354970203, 0.865003, 0.865003, 0.865003, 1.417541687274711, 0.705882, 0.0156863, 0.14902]
 velocityLUT.ScalarRangeInitialized = 1.0
+# Rescale transfer function
+velocityLUT.RescaleTransferFunction(0.0, 1.75)
 
 # trace defaults for the display properties.
 slice1Display.Representation = 'Surface'
 slice1Display.ColorArrayName = ['CELLS', 'Velocity']
 slice1Display.LookupTable = velocityLUT
-slice1Display.SelectTCoordArray = 'None'
-slice1Display.SelectNormalArray = 'None'
-slice1Display.SelectTangentArray = 'None'
-slice1Display.OSPRayScaleArray = 'Pressure'
-slice1Display.OSPRayScaleFunction = 'PiecewiseFunction'
 slice1Display.SelectOrientationVectors = 'Velocity'
 slice1Display.ScaleFactor = 0.01800000071525574
-slice1Display.SelectScaleArray = 'Pressure'
-slice1Display.GlyphType = 'Arrow'
-slice1Display.GlyphTableIndexArray = 'Pressure'
-slice1Display.GaussianRadius = 0.0009000000357627869
-slice1Display.SetScaleArray = ['POINTS', 'Pressure']
-slice1Display.ScaleTransferFunction = 'PiecewiseFunction'
-slice1Display.OpacityArray = ['POINTS', 'Pressure']
-slice1Display.OpacityTransferFunction = 'PiecewiseFunction'
 slice1Display.DataAxesGrid = 'GridAxesRepresentation'
-slice1Display.PolarAxes = 'PolarAxesRepresentation'
 slice1Display.SelectInputVectors = ['POINTS', 'Velocity']
 slice1Display.WriteLog = ''
-slice1Display.NoiseArray = ['POINTS', 'Pressure']
-slice1Display.BumpMapInputDataArray = ['POINTS', 'Pressure']
-slice1Display.ExtrusionInputDataArray = ['CELLS', 'Pressure']
-slice1Display.InputVectors = ['CELLS', 'Velocity']
-
-# init the 'PiecewiseFunction' selected for 'ScaleTransferFunction'
-slice1Display.ScaleTransferFunction.Points = [-288.5645751953125, 0.0, 0.5, 0.0, 1399.6429443359375, 1.0, 0.5, 0.0]
-
-# init the 'PiecewiseFunction' selected for 'OpacityTransferFunction'
-slice1Display.OpacityTransferFunction.Points = [-288.5645751953125, 0.0, 0.5, 0.0, 1399.6429443359375, 1.0, 0.5, 0.0]
 
 # setup the color legend parameters for each legend in this view
 
@@ -125,16 +100,6 @@ velocityLUTColorBar.Visibility = 1
 
 # show color legend
 slice1Display.SetScalarBarVisibility(renderView1, True)
-
-# ----------------------------------------------------------------
-# setup color maps and opacity mapes used in the visualization
-# note: the Get..() functions create a new object, if needed
-# ----------------------------------------------------------------
-
-# get opacity transfer function/opacity map for 'Velocity'
-velocityPWF = GetOpacityTransferFunction('Velocity')
-velocityPWF.Points = [0.12359778371932974, 0.0, 0.5, 0.0, 1.417541687274711, 1.0, 0.5, 0.0]
-velocityPWF.ScalarRangeInitialized = 1
 
 # ----------------------------------------------------------------
 # setup extractors
@@ -159,6 +124,7 @@ SetActiveSource(pNG1)
 # Catalyst options
 from paraview import catalyst
 options = catalyst.Options()
+options.ExtractsOutputDirectory = '.'
 options.GlobalTrigger = 'TimeStep'
 options.CatalystLiveTrigger = 'TimeStep'
 
